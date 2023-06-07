b9806c5ec6f1e0b4701afcffe0e582d2b448e523
Paiiapzzzzz <paiporn.pai@mail.kmutt.ac.th>
2023-06-07 21:36:46 +0700

    update

---
+++ src/routes/(dashboard)/dashboard/manager/+page.svelte
@@ -0,0 +1,161 @@
+<script>
+  import { Datepicker } from "flowbite-svelte";
+  import { Card } from "flowbite-svelte";
+  import Chart from "./chart.svelte";
+  import { DateInput } from "date-picker-svelte";
+    import { onMount } from "svelte";
+  let items;
+  let selected;
+  
+async function fetchItems() {
+    try {
+      const response = await fetch("http://localhost:5000/finance/PurchasingOrder", {
+        method: "POST",
+        body: JSON.stringify({
+          UserRole: "Operator",
+          Date: selected,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      const data = await response.json();
+      items = data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+    
+</script>
+
+<!-- <Datepicker  /> -->
+<main>
+  <!-- <Datepicker datepickerButtons /> -->
+  <!-- <Datepicker  /> -->
+  <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select </label>
+  <select bind:value={selected} on:change={() => fetchItems()} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option selected>DATE</option>
+    <option value="week1">week1</option>
+    <option value="week2">week2</option>
+    <option value="week3">week3</option>
+    <option value="week4">week4</option>
+    <option value="week5">week5</option>
+    <option value="month">monthly</option>
+
+  </select>
+  <div class="grid grid-cols-4 gap-8 mb-10 mt-10">
+    <Card>
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        Daily Diesel Volume
+      </p>
+    </Card>
+    <Card>
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        Daily Gas95 Volume
+      </p>
+    </Card>
+    <Card>
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        AVG Diesel Cycle Time
+      </p>
+    </Card>
+    <Card>
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        AVG GAS95 Cycle Time
+      </p>
+    </Card>
+  </div>
+  <div class="grid2 grid-cols-2 gap-8 mb-10 mt-10">
+    <div class="chart-container">
+      <p />
+      <Chart />
+    </div>
+    <div class="chart-container">
+      <p />
+      <Chart />
+    </div>
+  </div>
+  <div class="grid3 grid-cols-2 gap-16 center-cards mb-10 mt-10">
+    <Card>
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        Daily Diesel Volume
+      </p>
+    </Card>
+    <Card class="mr-10">
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        Daily Gas95 Volume
+      </p>
+    </Card>
+  </div>
+</main>
+
+<!-- <script>
+  import { Datepicker } from "flowbite-svelte";
+  import { Card } from "flowbite-svelte";
+  import Chart from "./chart.svelte";
+</script>
+
+<main>
+  <Chart />
+</main> -->
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 8px;
+  }
+  .grid2 {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 8px;
+  }
+  .grid3 {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 16px;
+  }
+  .chart-container {
+    max-width: 400px;
+    margin: 0 auto;
+  }
+  .center-code {
+    text-align: center;
+    
+  }
+  .center-cards {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
--- src/routes/(dashboard)/dashboard/operation/+page.svelte
+++ src/routes/(dashboard)/dashboard/operation/+page.svelte
@@ -1,138 +1,138 @@
-<script lang="ts">
-  import { Datepicker } from "flowbite-svelte";
+<script>
   import { Card } from "flowbite-svelte";
-  import { browser } from "$app/environment";
-  import { FY2021 as satisfactionData2021 } from "../../../../data.json";
-  import "@fontsource/merriweather";
-  import { Chart, registerables } from "chart.js";
-  import { onMount } from "svelte";
+  // import Chart from "./chart.svelte";
+  import { DateInput } from "date-picker-svelte";
+  import data from "../../../../data.json";
+  //import Chart from 'chart.js/auto';
+  const a = data;
+  let date = new Date();
+  date.setHours(0, 0, 0, 0);
+  let items = a;
 
-  Chart.register(...registerables);
-
-  let barChartElement: HTMLCanvasElement;
-
-  const chartData = {
-    labels: satisfactionData2021.map(({ framework }) => framework),
-    datasets: [
-      {
-        label: "Satisfaction (%)",
-        data: satisfactionData2021.map(({ score }) => score),
-        backgroundColor: [
-          "hsl(347 38% 49%)",
-          "hsl(346 65% 63%)",
-          "hsl(346 49% 56%)",
-          "hsl(346 89% 70%)",
-          "hsl(346 90% 76%)",
-          "hsl(346 90% 73%)",
-          "hsl(346 89% 79%)",
-          "hsl(346 89% 85%)",
-          "hsl(347 89% 82%)",
-          "hsl(346 90% 88%)",
-          "hsl(347 87% 94%)",
-          "hsl(347 91% 91%)",
-          "hsl(346 87% 97%)",
-        ],
-        borderColor: ["hsl(43 100% 52%)"],
-        borderRadius: 4,
-        borderWidth: 2,
-      },
-    ],
-  };
-
-  onMount(() => {
-    if (browser) {
-      new Chart(barChartElement, {
-        type: "bar",
-        data: chartData,
-        options: {
-          plugins: {
-            legend: {
-              display: false,
-            },
-          },
-          scales: {
-            x: {
-              grid: {
-                color: "hsl(43 100% 52% / 10%)",
-              },
-              ticks: { color: "hsl(43 100% 52% )" },
-            },
-            y: {
-              beginAtZero: false,
-              ticks: { color: "hsl(43 100% 52% )", font: { size: 18 } },
-              grid: {
-                color: "hsl(43 100% 52% / 40%)",
-              },
-              title: {
-                display: true,
-                text: "Satisfaction (%)",
-                color: "hsl(43 100% 52% )",
-                font: { size: 24, family: "Merriweather" },
-              },
-            },
-          },
+  async function fetchData() {
+    // Fetch the data based on the selected date
+    console.log(date);
+    try {
+      const response = await fetch("http://localhost:5000/finance/GeneralJournal", {
+        method: "POST",
+        body: JSON.stringify({
+          Date: date.toISOString(),
+          UserRole: "Manager"
+          
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
         },
       });
+      const data = await response.json();
+      items = data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-  });
+  }
 </script>
 
-<Datepicker />
+<main>
+  <DateInput bind:value={date} on:select={() => fetchData()} />
 
-<ul class="grid w-full h-full gap-8 md:grid-cols-4">
-  <Card href="/cards">
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      Noteworthy technology acquisitions 2021
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-      Daily Diesel Volume
-    </p>
-  </Card>
-  <Card href="/cards">
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      Noteworthy technology acquisitions 2021
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-      Daily Gas95 Volume
-    </p>
-  </Card>
-  <Card href="/cards">
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      Noteworthy technology acquisitions 2021
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-      AVG Diesel Cycle Time
-    </p>
-  </Card>
-  <Card href="/cards">
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      Noteworthy technology acquisitions 2021
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-      AVG GAS95 Cycle Time
-    </p>
-  </Card>
-</ul>
-<ul class="grid w-full h-full gap-8 md:grid-cols-2">
-  <main class="main-container">
-    <h1>State of JS 2021 Backend Framework Satisfaction</h1>
-    <section class="chart-container">
-      <canvas bind:this={barChartElement} />
-    </section>
-  </main>
-</ul>
+  <div class="grid grid-cols-3 gap-8 mb-10 mt-10">
+    {#each items as item}
+      <Card>
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {item.Amountdiesel}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+          Daily Diesel Volume
+        </p>
+      </Card>
+      <Card>
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {item.Amountgas95}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+          Daily Gas95 Volume
+        </p>
+      </Card>
+      <Card>
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {item.avgcycletime}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+          AVG Cycle Time
+        </p>
+      </Card>
+    {/each}
+  </div>
+  <!-- 
+  <div class="grid2 grid-cols-2 gap-8 mb-10 mt-10">
+    <div class="chart-container">
+      <p />
+      <Chart  />
+    </div>
+    <div class="chart-container">
+      <p />
+      <Chart  />
+    </div>
+  </div> -->
+
+  <div class="grid3 grid-cols-2 gap-16 center-cards mb-10 mt-10">
+    {#each items as item}
+      <Card>
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {item.Notruckin}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+          Truck in
+        </p>
+      </Card>
+      <Card class="mr-10">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {item.Notruckout}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+          Truck out
+        </p>
+      </Card>
+    {/each}
+  </div>
+</main>
 
 <style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 8px;
+  }
+  .grid2 {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 8px;
+  }
+  .grid3 {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 16px;
+  }
   .chart-container {
     max-width: 400px;
     margin: 0 auto;
+  }
+  .center-code {
+    text-align: center;
+  }
+  .center-cards {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
